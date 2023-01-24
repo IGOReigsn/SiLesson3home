9158f16ebe714d2392af36cb5639a0a53e1db4fc
IGOReigsn <eigsn@yandex.ru>
2023-01-24 11:47:35 +0300

    Tasks worked /draft

--- Task21/Program.cs
+++ Task21/Program.cs
@@ -2,3 +2,20 @@
 Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53*/
+
+Console.WriteLine("Введите координаты первой точки:");
+Console.Write("                       Х:");
+double X1=Convert.ToDouble(Console.ReadLine());
+Console.Write("                       Y:");
+double Y1=Convert.ToDouble(Console.ReadLine());
+Console.Write("                       Z:");
+double Z1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты второй точки:");
+Console.Write("                       Х:");
+double X2=Convert.ToDouble(Console.ReadLine());
+Console.Write("                       Y:");
+double Y2=Convert.ToDouble(Console.ReadLine());
+Console.Write("                       Z:");
+double Z2=Convert.ToDouble(Console.ReadLine());
+double result=Math.Pow(Z2,2);
+Console.WriteLine("Расстояние между точками:"+ result);
--- Task23/Program.cs
+++ Task23/Program.cs
@@ -2,3 +2,12 @@
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125*/
+
+Console.WriteLine("Введите число:");
+double num=Convert.ToDouble(Console.ReadLine());
+if (num>0)
+    {for(double i=1;i<=num;i++)
+        {Console.Write( i + " => "+ Math.Pow(i,3)+ "    ");}
+    }
+else {Console.Write("Введенное число должно быть положительным ");}
+Console.WriteLine();
